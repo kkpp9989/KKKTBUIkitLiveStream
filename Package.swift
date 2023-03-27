fa7f98f0d588b0cf50e8fc8aabb1a204f9d3323c
kkpp9989 <117346116+kkpp9989@users.noreply.github.com>
2023-03-27 09:17:55 +0700

    Update Package.swift
    update AmityUIKitLiveStream 2.35.3

--- Package.swift
+++ Package.swift
@@ -14,18 +14,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmityUIKitLiveStream",
-            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.35.1/AmityUIKitLiveStream.xcframework.zip",
-            checksum: "19d9d27ea10254c33343252229da5ab93d7637709152970bba298f306d3733d3"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.35.3/AmityUIKitLiveStream.xcframework.zip",
+            checksum: "64c30cefafcd82a8d97b37eed69a84dfe9d8415b70a07d2ac5671528d3fbae93"
         ),
         .binaryTarget(
             name: "AmityLiveVideoBroadcastKit",
-            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.35.1/AmityLiveVideoBroadcastKit.xcframework.zip",
-            checksum: "80673a8695a8b746cd0e4a37cd52d8d8439abb0aef6317d5c5b477f4a0fa5e04"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.35.3/AmityLiveVideoBroadcastKit.xcframework.zip",
+            checksum: "027cf46134e5358237dd689eab9a70aad12a09ad6595d88bfd4fb845586f49cc"
         ),
         .binaryTarget(
             name: "AmityVideoPlayerKit",
-            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.35.1/AmityVideoPlayerKit.xcframework.zip",
-            checksum: "60557dd8741d9843b21292545f2341ec2ac5ab5146f8006d8785a3e5680ddae4"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.35.3/AmityVideoPlayerKit.xcframework.zip",
+            checksum: "fb4485ca3fa6f4a85cfd2c0560b4eab2749d3a29268c76dfab98eb7ad7916497"
         ),
         .binaryTarget(
             name: "MobileVLCKit",
